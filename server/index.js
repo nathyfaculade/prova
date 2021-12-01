519cc034b334e947ac7ae71d3a9c2188c7ad4563
Nathy <91018534+nathyfaculade@users.noreply.github.com>
2021-12-01 18:33:29 -0300

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const groupRouter = require('./routes/group-router');
+const productRouter = require('./routes/product-router');
+
+const app = express();
+
+const PORT = 3333;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//Rota principal
+app.get('/', (req, res) => {
+    res.send(`It's working!`);
+});
+
+//Demais rotas
+app.use('/groups', groupRouter);
+app.use('/products', productRouter);
+
+app.listen(PORT, () => {
+    console.log(`Executando na porta ${PORT}`);
+});
